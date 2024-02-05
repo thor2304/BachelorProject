febae6076d143f182c30c4cd3f45a5451be1601d
TobiasBak <tobib21@student.sdu.dk>
2024-02-05 12:25:36 +0100

    exclude node_modules

--- js_client/webpack.config.js
+++ js_client/webpack.config.js
@@ -21,10 +21,12 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+                exclude: /node_modules/,
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
+                exclude: /node_modules/,
             },
         ],
     },
