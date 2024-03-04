20e45a552428695d802af779eb749cfdb5d4cdbe
Kevin Torp <Kevintorp@hotmail.com>
2024-03-04 10:36:56 +0100

    Added checks for input history on arrow keys
    Added check which checks if the curser is on the first or last line before it will navigate in the chat history

--- js_client/src/input.ts
+++ js_client/src/input.ts
@@ -1,8 +1,9 @@
-const inputField: HTMLInputElement = <HTMLInputElement> document.getElementById("inputField")
+const inputField: HTMLInputElement = <HTMLInputElement>document.getElementById("inputField")
 export const commandList: HTMLElement = document.getElementById("commandList");
 
 const commandHistory: string[] = [];
 let historyIndex: number = 0;
+let cursorPosRowNumber: number;
 
 function getTextFromInput2(): string {
     const text: string = inputField.value.trim();
@@ -10,7 +11,7 @@ function getTextFromInput2(): string {
     return text;
 }
 
-function createPTagWithText2(text: string, id: number): void{
+function createPTagWithText2(text: string, id: number): void {
     const liElement: HTMLLIElement = document.createElement('li');
     liElement.id = `command-${id}`;
     liElement.textContent = text;
@@ -19,17 +20,45 @@ function createPTagWithText2(text: string, id: number): void{
 }
 
 function saveCommandToHistory(command: string): void {
-    if(command != ''){
+    if (command != '') {
         commandHistory.push(command);
         historyIndex = commandHistory.length;
     }
+}
+
+enum targetDirection {
+    up = 'up',
+    down = 'down',
+}
+
+function isCursorOnLine(textarea: HTMLInputElement, direction: targetDirection): boolean {
+    const cursorPos = textarea.selectionStart;
+    const selectionEnd = textarea.selectionEnd;
+    const selectionStart = textarea.selectionStart;
+
+    if (selectionStart !== selectionEnd) {
+        return false;
+    }
+
+    const firstNewLine = textarea.value.indexOf('\n');
+    const lastNewLine = textarea.value.lastIndexOf('\n');
+
+    if ((cursorPos <= firstNewLine || firstNewLine < 0) && direction === targetDirection.up) {
+        return true;
+    }
+
+    if (cursorPos > lastNewLine && direction === targetDirection.down) {
+        return true;
+    }
+
+    return false;
 }
 
 let current_id: number = 0;
 inputField.addEventListener('keydown', function (e: KeyboardEvent): void {
     switch (e.key) {
         case 'Enter':
-            if(e.key === 'Enter' && e.shiftKey) return;
+            if (e.key === 'Enter' && e.shiftKey) return;
             const customEvent: CustomEvent<{ text: string }> = new CustomEvent('commandEntered', {
                 detail: {
                     text: getTextFromInput2(),
@@ -41,19 +70,21 @@ inputField.addEventListener('keydown', function (e: KeyboardEvent): void {
             document.dispatchEvent(customEvent);
             break;
         case 'ArrowUp':
-            if(e.key === 'ArrowUp' && e.shiftKey) return;
-            e.preventDefault();
-            if (commandHistory.length > 0) {
-                historyIndex = (historyIndex === 0) ? commandHistory.length - 1 : --historyIndex;
-                inputField.value = commandHistory[historyIndex];
+            if (isCursorOnLine(this, targetDirection.up) || inputField.value === '') {
+                e.preventDefault();
+                if (commandHistory.length > 0) {
+                    historyIndex = (historyIndex === 0) ? commandHistory.length - 1 : --historyIndex;
+                    inputField.value = commandHistory[historyIndex];
+                }
             }
             break;
         case 'ArrowDown':
-            if(e.key === 'ArrowDown' && e.shiftKey) return;
-            e.preventDefault();
-            if (commandHistory.length > 0 && historyIndex < commandHistory.length ) {
-                historyIndex = (historyIndex === commandHistory.length - 1) ? 0 : ++historyIndex;
-                inputField.value = commandHistory[historyIndex];
+            if (isCursorOnLine(this, targetDirection.down) || inputField.value === '') {
+                e.preventDefault();
+                if (commandHistory.length > 0 && historyIndex < commandHistory.length) {
+                    historyIndex = (historyIndex === commandHistory.length - 1) ? 0 : ++historyIndex;
+                    inputField.value = commandHistory[historyIndex];
+                }
             }
             break;
     }
